1ca0b1acb3eda8989ceb07bef91eeeca40f9ad5d
Atul Singh <atulsingh28092005@gmail.com>
2025-12-27 19:44:42 +0530

    single element code

--- LeetCode/SingleElementSortedArray.java
+++ LeetCode/SingleElementSortedArray.java
@@ -2,69 +2,64 @@ package LeetCode;
 
 public class SingleElementSortedArray 
 {
-    public static void main(String[] args) {
-        int arr[]={1,1,2,3,3,4,4,8,8};
-    int left=SingleElement(arr);
-    int right= SingleElement2(arr);
-    if(left!=-1){
-        System.out.println(left);
-    }else{
-        System.out.println(right);
-    }
+    public static void main(String[] args) 
+    {
+        int nums[] = {3,3,7,7,10,11,11};
+        int result = SingleElement(nums);
+        System.out.println(result);
 
+        
     }
-
-    static int SingleElement(int arr[])
+    
+    static int SingleElement(int nums[])
     {
         int start = 0;
-        int end  =arr.length-1;
+        int end =  nums.length-1;
 
-        while(start <= end)
+        while(start < end)
         {
-            int mid = start+(end-start)/2;
+            int mid = start +(end-start)/2;
+            
+            Boolean isEven;
 
-            if(mid>start && arr[mid] == arr[mid-1])
-            {   System.out.println("left:"+end);
-                end= mid-2;
-            }
-            if((mid<end && arr[mid]<arr[mid+1]) && (arr[mid]>arr[mid-1] && start>mid))
+            if((end-mid)%2 == 0)
             {
-                return mid;
-            }else if(start>mid){
-                System.out.println("left:"+end);
-                end=mid-1;
+               isEven = true;
+            }
+            else
+            {
+                isEven = false;
             }
 
+
+            if(nums[mid] == nums[mid+1])
+            {
+
+                if(isEven)
+                {
+                    start = mid+2;
+                }
+                else
+                {
+                    end = mid-1;
+                }
+            }
+            else if(nums[mid] != nums[mid+1])
+            {
+                if(isEven)
+                {
+                    end =mid;
+                }
+                else
+                {
+                   start = mid+1;
+                }
+
+            }
         }
-        return -1;
+        return nums[end];
 
     }
 
-    static int SingleElement2(int arr[])
-    {
-        int start = 0;
-        int end  =arr.length-1;
-
-        while(start <= end)
-        {
-            int mid = start+(end-start)/2;
-
-            if(mid>end && arr[mid] == arr[mid+1])
-            {  System.out.println("Right:"+start);
-                start= mid+2;
-            }
-            if((mid<=end && arr[mid]<arr[mid+1]) && (arr[mid]>arr[mid-1] && start>=mid))
-            {
-                return mid;
-            }else if(end>mid){
-                 System.out.println("Right:"+start);
-                start=mid+1;
-            }
-
-
-        }
-        return -1;
-
-    }
     
 }
